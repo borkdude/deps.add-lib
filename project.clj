d7d56431663d0f0fec5168eede7bc6df65718af1
Michiel Borkent <michielborkent@gmail.com>
2023-05-13 21:16:13 +0200

    release info

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject io.github.borkude/deps.add-lib "0.0.1"
   :dependencies [[org.clojure/clojure "1.12.0-alpha2"]
-                 [borkdude/deps.clj "1.11.1.1273-3"]])
+                 [borkdude/deps.clj "1.11.1.1273-3"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
